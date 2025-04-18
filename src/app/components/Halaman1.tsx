196c0e27c2aceef721a359eb8b679757bc26ccf1
Farhan Franaka <farhanfranaka0505@gmail.com>
2025-04-18 10:54:46 +0700

    fix: build

--- src/app/components/Halaman1.tsx
+++ src/app/components/Halaman1.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 import Header from "./Header";
 import Footer from "./Footer";
 
--- src/app/components/Halaman2.tsx
+++ src/app/components/Halaman2.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
-import Penilaian from "./PenilaianHasil";
 import PenilaianContoh from "./PenilaianContoh";
 
 function Halaman2() {
